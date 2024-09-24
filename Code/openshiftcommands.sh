f7fd7481fe94734f61921b58f37fb5624e050839
Erdem <rifaterdemsahin@gmail.com>
2024-09-24 20:04:40 +0100

    Update openshiftcommands.sh

--- Code/openshiftcommands.sh
+++ Code/openshiftcommands.sh
@@ -1,4 +1,9 @@
 oc apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/main/bundle.yaml
 oc create namespace monitoring
 oc apply -f prometheus-instance.yaml
-
+oc apply -f thanos-sidecar.yaml
+oc apply -f thanos-config.yaml
+oc apply -f thanos-receiver.yaml
+oc apply -f thanos-query.yaml
+oc apply -f thanos-query-service.yaml
+oc apply -f thanos-store.yaml
